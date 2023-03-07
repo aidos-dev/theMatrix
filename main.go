986e5a5606373d4933be5457c35319506c26b20d
Optimus <3300532@gmail.com>
2023-03-07 11:02:37 +0600

    white spaces

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"strings"
 	"time"
 
 	"theMatrix/pkg/console"
@@ -24,7 +23,7 @@ func main() {
 		go console.ConsoleSize(consoleSize)
 
 		go lineGenerator(symbols, consoleSize, oneLine)
-		time.Sleep(time.Second)
+		time.Sleep(time.Millisecond * 700)
 		fmt.Println(<-oneLine)
 	}
 }
@@ -35,30 +34,25 @@ func lineGenerator(symbols map[int]string, consoleSize chan int, oneLine chan st
 
 	termSize := <-consoleSize
 
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
+	// spaceNumFlag variable is the ratio of white spaces to symbols
+	// here it is 6/10
+	spaceNumFlag := (len(symbols) / 10) * 6
 
-	// spaceNum variable contains the possible number of white spaces in a row of symbols
-	spaceNum := termSize / 10
-
-	// this loop creates a string of random symbols  with the size of terminal window
+	// this loop creates a string of random symbols and white spaces between symbols in random places
+	// with the size of terminal window
 	for i := 0; i < termSize; i++ {
+
+		s1 := rand.NewSource(time.Now().UnixNano())
+		r1 := rand.New(s1)
 
 		rand := r1.Intn(len(symbols))
 
-		res += symbols[rand]
+		if rand >= spaceNumFlag {
+			res += " "
+		} else {
+			res += symbols[rand]
+		}
 
-	}
-
-	// this loop replaces some symbols with white spaces in random places
-	for i := 0; i < spaceNum; i++ {
-
-		s2 := rand.NewSource(time.Now().UnixNano())
-		r2 := rand.New(s2)
-
-		spaceRand := r2.Intn(termSize)
-
-		res = strings.Replace(res, string(res[spaceRand]), " ", 1)
 	}
 
 	oneLine <- res
